7f084f44b966552c66eddbb64bdb778431a402de
CapableNewspaper <jared.heymann@bigpond.com>
2024-05-06 23:46:29 +1000

    reeeh

--- lua/inking-latex/init.lua
+++ lua/inking-latex/init.lua
@@ -1,20 +1,79 @@
 local M = {}
 
-local func = require("inking-latex.functions")
+-- I gave up on multifile one
+-- local func = require("inking-latex.functions")
 
--- get template location; should be located in a figures folder in the main directory called template
+function M.CreateFile(template,filename)
+  -- os.execute("cp \"" .. template .. " " .. filename)
+  os.execute("cp ".. template .. " "  .. filename )
+end
+
+function M.OpenFile(file)
+  os.execute("open " .. file)
+end
+
+function M.CreateOpenFile(template,newFileName)
+  -- Get the name of the current buffer
+  local current_buffer_name = vim.api.nvim_buf_get_name(0)
+
+  -- Extract the directory path from the buffer name
+  local current_directory = current_buffer_name:match("(.*[/\\])")
+
+  -- Prompt the user for the name of the new file
+  local newFile = current_directory .."/figures/" .. newFileName
+
+  -- Create a new file based on a template
+  M.CreateFile(template, newFile)
+
+  -- Open the new file
+  M.OpenFile(newFile)
+end
+
+function M.RelativePath()
+  -- Get the name of the current buffer
+  local buffer_name = vim.api.nvim_buf_get_name(0)
+
+  -- Get the directory of the current buffer
+  local buffer_directory = vim.fn.fnamemodify(buffer_name, ":h")
+
+  -- Get the current working directory
+  local working_directory = vim.fn.getcwd()
+
+  -- Calculate the relative path
+  local relative_path = vim.fn.relpath(buffer_directory, working_directory)
+
+  return relative_path
+end
+
+function M.PrintFigure(fileName)
+  local buf = vim.api.nvim_get_current_buf()
+
+  local current_win = vim.api.nvim_get_current_win()
+  local cursor = vim.api.nvim_win_get_cursor(current_win)
+  local current_line = cursor[1]
+  print(current_line)
+  -- Set the new text for the entire buffer
+  local my_text ={
+      '\\begin{figure}[h]',
+      '    \\centering',
+      '    \\includesvg[width=0.8\\textwidth]{'.. fileName ..'}',
+      '\\end{figure}'
+    }
+  vim.api.nvim_buf_set_lines(buf, current_line-1, current_line,  false, my_text)
+end
 
 
 function M.CreateFigure()
-
   local working_directory = vim.fn.getcwd()
   local template = working_directory .."/figures/template.svg"
   -- print(template)
 
   local newFileName= vim.fn.input("Enter the name for the new file (no extension): ") .. ".svg"
 
-  func.CreateOpenFile(template,newFileName)
+  M.CreateOpenFile(template,newFileName)
 
 end
 
 return M
+
+
